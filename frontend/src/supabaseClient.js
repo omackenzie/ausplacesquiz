ca51effc5e672ff15b02e22df6e9d3fa477a9113
Oscar MacKenzie <oscarmac384@gmail.com>
2023-01-06 15:27:28 +1100

    remove console.log

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -3,7 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 let supabaseURL = import.meta.env.VITE_SUPABASE_URL;
 let supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log(supabaseURL);
-console.log(supabaseAnonKey);
-
 export const supabase = createClient(supabaseURL, supabaseAnonKey);
